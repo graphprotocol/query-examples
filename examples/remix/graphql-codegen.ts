d4aba68223a2d8f2c2b8f2d7e3d60fb945673612
Florin Georgescu <flo_georgescu@hotmail.com>
2025-03-06 13:55:08 +0200

    (feat): Updated exampls for codegen with API key in headers

--- examples/remix/graphql-codegen.ts
+++ examples/remix/graphql-codegen.ts
@@ -13,8 +13,14 @@ export default {
   generates: {
     './app/generated/': {
       schema: [
-        // replace this with your Subgrah URL
-        `https://gateway-arbitrum.network.thegraph.com/api/${API_KEY}/subgraphs/id/DZz4kDTdmzWLWsV373w2bSmoar3umKKH9y82SUKr5qmp`,
+        {
+          // replace this with your Subgrah URL
+          'https://gateway-arbitrum.network.thegraph.com/api/subgraphs/id/DZz4kDTdmzWLWsV373w2bSmoar3umKKH9y82SUKr5qmp': {
+            headers: {
+              Authorization: `Bearer ${API_KEY}`,
+            },
+          },
+        },
       ],
       documents: ['./app/**/*.{ts,tsx}'],
       preset: 'client',
